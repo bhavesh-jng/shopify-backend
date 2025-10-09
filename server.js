12fb925b51de1da1b6b5c09df88c748a010581f9
unknown <nishant@jnitin.com>
2025-10-09 16:01:05 +0530

    test2

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.use(cors({ origin: allowedOrigins }));
 
 // --- App Proxy Routes (must be mounted at /apps/proxy) ---
 // These routes are accessed via: yourstore.myshopify.com/apps/proxy/...
-app.use("customers", customers);
+app.use("/proxy/customers", customers);
 
 // --- Regular API Routes (non-proxy) ---
 app.use("/update-customer-metafields", updateCustomerRoutes);
